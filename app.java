a60eec346df6327a703e53f5e4ddcedf87b5de07
gargi123-cloud <2021wb86263@wilp.bits-pilani.ac.in>
2025-07-14 22:23:46 +0530

    Create app.java

---
+++ app.java
@@ -0,0 +1,12 @@
+package com.devops.lab;
+
+public class App
+{ 
+  public static void main(string[] arg )
+  {
+    System.out.println( "Hello From Continuous Integration Trigger|");
+    System.out.println( "Devops lab 4 complete if the first line of output shows Started by SCM");
+    System.out.println( "Hello from CI");
+    System.out.println( 2 + 2 );
+  }
+}
